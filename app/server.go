47154b0e06c6929ee6678f637560510d902cd39e
Roman Baldaev <baldaev.roman@gmail.com>
2024-09-24 23:10:36 +0700

    Remove comments

--- app/server.go
+++ app/server.go
@@ -13,11 +13,7 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	//
+	// run server
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -25,6 +21,7 @@ func main() {
 	}
 	defer l.Close()
 
+	// accept connection
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
